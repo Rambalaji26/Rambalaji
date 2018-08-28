84f74a8c5be8adf6332ba30f7a4145a7ce0d3813
Rambalaji26 <42169880+Rambalaji26@users.noreply.github.com>
2018-08-28 14:04:30 +0530

    Create PosorNeg.java

---
+++ PosorNeg.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class PosorNeg
+{
+  piublic static void main(String args[])\
+   {
+     int n;
+     Scanner s=new Scanner(System.in);
+     System.out.println("enter the number");
+     n=s.nextint();
+     if(n>0)
+         {
+            System.out.println("The number is Positive");
+         }
+     else if(n<0)
+         {
+            System.out.println("The number is negative");
+         }
+     else
+         {
+           System.out.println("the number is zero");
+         }
+      }
+   }
+          
+  
