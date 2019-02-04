b79175eec4af6e344b42ab611e6eb6f8dccc35ef
Rambalaji26 <42169880+Rambalaji26@users.noreply.github.com>
2019-02-04 14:03:10 +0530

    Update PosorNeg.java

--- PosorNeg.java
+++ PosorNeg.java
@@ -5,21 +5,18 @@ class PosorNeg
    {
      int n;
      Scanner s=new Scanner(System.in);
-     System.out.println("enter the number");
      n=s.nextInt();
      if(n>0)
          {
-            System.out.println("The number is Positive");
+            System.out.println("Positive");
          }
      else if(n<0)
          {
-            System.out.println("The number is negative");
+            System.out.println("Negative");
          }
      else
          {
-           System.out.println("the number is zero");
+           System.out.println("Zero");
          }
       }
    }
-          
-  
